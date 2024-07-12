60f3b977df2f959985212088155f8c02dd71fe54
Edward Derks <edward.derks@gmail.com>
2024-07-12 08:33:19 +0200

    feature: treemap rendered, awaiting 1) prop allocation per node; 2) prop derived info per node (text); 3) styling; 4) tooltip: transformation/animation upon hover: enlarging target node

--- index.js
+++ index.js
@@ -19,10 +19,8 @@ const games = await getData(gamesURL_static);
 // const movies = await getData(moviesURL);
 // const games = await getData(gamesURL);
 
+//flatten kickstart object
 const kickstartFlat = kickstart.children.map((k) => k.children).flat();
-
-console.log(kickstart.children);
-console.log(kickstartFlat);
 
 //create nested map object 'groups'
 const groups = d3.rollup(
@@ -35,10 +33,34 @@ const groups = d3.rollup(
 //feed groups to hierarchy-method to create D3's hierarchy-structure 'root'
 const root = d3.hierarchy(groups);
 
-//hierarchy's sum method propagates
+//hierarchy's sum method propagates associated value up the tree
 root.sum((d) => d[1]);
 
-console.log(root);
+//create treemap
+const treemap = d3.treemap();
+
+//set treemap layout
+treemap.size([100, 100]).padding(1);
+
+//call treemap with hierarchy object 'root'
+treemap(root);
+
+//create svg
+d3.select("#app")
+  .append("svg")
+  .attr("id", "chart")
+  .attr("viewBox", "0 0 100 100")
+  .selectAll("rect")
+  .data(root.leaves())
+  .enter()
+  .append("rect")
+  .attr("x", (d) => d.x0)
+  .attr("y", (d) => d.y0)
+  .attr("width", (d) => d.x1 - d.x0)
+  .attr("height", (d) => d.y1 - d.y0)
+  .style("fill", "none")
+  .style("stroke", "black")
+  .style("stroke-width", 0.1);
 
 // total funds verification
 const verify = (input) => {
@@ -51,5 +73,9 @@ const verify = (input) => {
   return sum;
 };
 
+//logs
+console.log(kickstart.children);
+console.log(kickstartFlat);
 console.log("d3 computed total funds", sumOfFunds(kickstartFlat));
 console.log("verified total funds", verify(kickstart.children));
+console.log(root);
